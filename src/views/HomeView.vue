c34d24327f1ad7044370b1b72bb63c4dec634d7a
Thushalya <weeeratharava@gmail.com>
2022-04-28 20:05:09 +0530

    initializing vuejs-recipe

---
+++ src/views/HomeView.vue
@@ -0,0 +1,25 @@
+<script setup>
+import TheWelcome from '@/components/TheWelcome.vue'
+</script>
+
+<template>
+  <div class="home">
+    <!-- <TheWelcome /> -->
+    <h1>My Recipies</h1>
+  </div>
+  
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .home {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+
+  .h1{
+    color: red;
+  }
+}
+</style>
